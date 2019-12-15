98721b2c1b1e5d8f8765640bd58840fa40370e54
RobertBonagura <Robert.Bonagura@gmail.com>
2019-12-14 19:05:50 -0800

    Implement gen, add and show Linked List functions

--- data_structures.c
+++ data_structures.c
@@ -1,8 +1,43 @@
 #include "data_structures.h"
 
 // Function Definitions
-*Linked_List gen_list(){
-    Linked_List list = malloc(sizeof(Linked_List));
-    Linked_List* list_ptr = &list;
+Linked_List* gen_list(){
+    
+    Linked_List* list_ptr = malloc(sizeof(Linked_List));
+    list_ptr->next = NULL;  
+    list_ptr->prev = NULL;
+    list_ptr->data = NULL;
+    list_ptr->size = 0;
+    
     return list_ptr;
 }
+
+int add(Linked_List *list, Node *node){
+
+    Linked_List* cur_list = list;
+    while (cur_list->data){
+        cur_list->prev = cur_list;
+        cur_list = cur_list->next;
+    }
+    cur_list->data = node;
+    cur_list->next = malloc(sizeof(node));
+    cur_list->next->prev = cur_list;
+
+    list->size++;
+    return 1;
+    
+}
+
+void show(Linked_List* list){
+
+    Linked_List *cur_list = list;
+    printf("The Linked List has %d nodes:\n\n", list->size);
+
+    int i = 0;
+    while (cur_list->data){
+        printf("Node %d: %s\n", i, cur_list->data->str);
+	cur_list = cur_list->next;
+	i++;
+    }
+}
+
--- main.c
+++ main.c
@@ -1,8 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "data_structures.h"
 
 int main(int argc, char *argv[]){
 
     printf("Hello World\n");
+
+    Linked_List *list = gen_list();
+
+    Node *node1 = malloc(sizeof(Node));
+    Node *node2 = malloc(sizeof(Node));
+    Node *node3 = malloc(sizeof(Node));
+    Node *node4 = malloc(sizeof(Node));
+    Node *node5 = malloc(sizeof(Node));
+
+    node1->str = "First";
+    node2->str = "Second";
+    node3->str = "Thirsty thirdly";
+    node4->str = "May the fourth be with you";
+    node5->str = "Fifth";
+
+    add(list, node1);
+    add(list, node2);
+    add(list, node3);
+    add(list, node4);
+    add(list, node5);
+
+    show(list);
 
     return 1;
 }
